ecc2eb3c7d9d62042a1aedc1f29483d880093e3d
H2O2777 <140376142+H2O2777@users.noreply.github.com>
2023-09-30 14:58:01 +0300

    Считывание и запись в файлы

--- Samokrutov_AC2204/Samokrutov_AC2204/Samokrutov_AC2204.cpp
+++ Samokrutov_AC2204/Samokrutov_AC2204/Samokrutov_AC2204.cpp
@@ -1,5 +1,6 @@
 ﻿#include <iostream>
 #include <string>
+#include <fstream>
 
 using namespace std;
 
@@ -43,6 +44,28 @@ void EditTruba(Truba& s)
 	s.dlina -= 1;
 }
 
+Truba LoadTruba()
+{
+	ifstream in;
+	in.open("Truba.txt",ios::in);
+	Truba s;
+	in >> s.nameT;
+	in >> s.dlina;
+	in >> s.diametr;
+	in.close();
+	return s;
+}
+
+void SaveTruba(const Truba& s)
+{
+	ofstream out;
+	out.open("OTBET.txt", ios::out);
+	out << "Truba: " << s.nameT
+		<< "\nLong: " << s.dlina
+		<< "\nDiameter: " << s.diametr << endl;
+	out.close();
+}
+
 KS InputKS()
 {
 	KS s;
@@ -63,14 +86,36 @@ void PrintKS(KS s)
 		<< "\nWorking workshop: " << s.wceh << endl;
 }
 
+KS LoadKS()
+{
+	ifstream in;
+	in.open("KS.txt", ios::in);
+	KS s;
+	in >> s.nameKS;
+	in >> s.aceh;
+	in >> s.wceh;
+	in.close();
+	return s;
+}
+
+void SaveKS(KS s)
+{
+	ofstream out;
+	out.open("OTBET.txt", ios::app );
+	out << "Kompressor Station: " << s.nameKS
+		<< "\nAll workshop: " << s.aceh
+		<< "\nWorking workshop: " << s.wceh << endl;
+	out.close();
+}
+
 int main()
 {
-	Truba tr = InputTruba();
-	PrintTruba(tr);
+	Truba tr = LoadTruba();
+	SaveTruba(tr);
 	//EditTruba(tr);
 	//PrintTruba(tr);
-	KS ks = InputKS();
-	PrintKS(ks);
+	KS ks = LoadKS();
+	SaveKS(ks);
 
 	return 0;
 }
