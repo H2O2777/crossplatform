6d452c3d33f55de77acf30337d2cae62dcfb084d
H2O2777 <140376142+H2O2777@users.noreply.github.com>
2023-09-30 12:57:17 +0300

    Добавлены структуры и вводы и выводы для них

--- Samokrutov_AC2204/Samokrutov_AC2204/Samokrutov_AC2204.cpp
+++ Samokrutov_AC2204/Samokrutov_AC2204/Samokrutov_AC2204.cpp
@@ -1,12 +1,69 @@
-﻿
+﻿#include <iostream>
+#include <string>
 
-#include <iostream>
+using namespace std;
+
+struct Truba
+{
+	string nameT;
+	int dlina;
+	int diametr;
+	string repair;
+};
+
+struct KS
+{
+	string nameKS;
+	int aceh;
+	int wceh;
+	int kpd;
+};
+
+Truba InputTruba()
+{
+	Truba s;
+	cout << "Truba name: ";
+	cin >> s.nameT;
+	cout << "Truba long: ";
+	cin >> s.dlina;
+	cout << "Truba diameter: ";
+	cin >> s.diametr;
+	return s;
+}
+
+void PrintTruba(Truba s)
+{
+	cout << "Truba: " << s.nameT
+		<< "\nLong: " << s.dlina
+		<< "\nDiameter: " << s.diametr << endl;
+}
+
+KS InputKS()
+{
+	KS s;
+	cout << "Kompressor Station name: ";
+	cin >> s.nameKS;
+	cout << "All workshop: ";
+	cin >> s.aceh;
+	cout << "Working workshop: ";
+	cin >> s.wceh;
+	return s;
+}
+
+
+void PrintKS(KS s)
+{
+	cout << "Kompressor Station: " << s.nameKS
+		<< "\nAll workshop: " << s.aceh
+		<< "\nWorking workshop: " << s.wceh << endl;
+}
 
 int main()
 {
-    int some;
-    std::cout << "Hello World!\n";
-    std::cout << "Скажите что-нибудь: ";
-    std::cin >> some;
-}
+	Truba tr = InputTruba();
+	PrintTruba(tr);
+	KS ks = InputKS();
+	PrintKS(ks);
 
+	return 0;
+}
